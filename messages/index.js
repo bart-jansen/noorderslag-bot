b64ae62c7a074171ef6b902ed64f190f7253aa28
Bart Jansen <b@rtjansen.nl>
2017-01-05 13:38:17 +0100

    lazy

--- messages/index.js
+++ messages/index.js
@@ -42,11 +42,19 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     },
     function (session, results) {
         if (results.response) {
-            // ... save task
-            var foundEvent = DataLayer.getArtist(results.response);
+            // // ... save task
+            // var foundEvent = DataLayer.getArtist(results.response);
 
-            // create the card based on selection
-            var card = createCard(foundEvent, session);
+            // // create the card based on selection
+            // var card = createCard(foundEvent, session);
+
+            // // attach the card to the reply message
+            // var msg = new builder.Message(session).addAttachment(card);
+            // session.send(msg);
+
+                    // create the card based on selection
+            var selectedCardName = 'Hero card';
+            var card = createCard(selectedCardName, session);
 
             // attach the card to the reply message
             var msg = new builder.Message(session).addAttachment(card);
@@ -68,14 +76,73 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 
 bot.dialog('/', intents);
 
+const HeroCardName = 'Hero card';
+const ThumbnailCardName = 'Thumbnail card';
+const ReceiptCardName = 'Receipt card';
+const SigninCardName = 'Sign-in card';
+const CardNames = [HeroCardName, ThumbnailCardName, ReceiptCardName, SigninCardName];
 
-function createCard(event, session) {
+function createCard(selectedCardName, session) {
+    switch (selectedCardName) {
+        case HeroCardName:
+            return createHeroCard(session);
+        case ThumbnailCardName:
+            return createThumbnailCard(session);
+        case ReceiptCardName:
+            return createReceiptCard(session);
+        case SigninCardName:
+            return createSigninCard(session);
+        default:
+            return createHeroCard(session);
+    }
+}
+
+function createHeroCard(session) {
     return new builder.HeroCard(session)
         .title('BotFramework Hero Card')
         .subtitle('Your bots — wherever your users are talking')
         .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
         .images(getSampleCardImages(session))
         .buttons(getSampleCardActions(session));
+}
+
+function createThumbnailCard(session) {
+    return new builder.ThumbnailCard(session)
+        .title('BotFramework Thumbnail Card')
+        .subtitle('Your bots — wherever your users are talking')
+        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .images(getSampleCardImages(session))
+        .buttons(getSampleCardActions(session));
+}
+
+var order = 1234;
+function createReceiptCard(session) {
+    return new builder.ReceiptCard(session)
+        .title('John Doe')
+        .facts([
+            builder.Fact.create(session, order++, 'Order Number'),
+            builder.Fact.create(session, 'VISA 5555-****', 'Payment Method'),
+        ])
+        .items([
+            builder.ReceiptItem.create(session, '$ 38.45', 'Data Transfer')
+                .quantity(368)
+                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png')),
+            builder.ReceiptItem.create(session, '$ 45.00', 'App Service')
+                .quantity(720)
+                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png'))
+        ])
+        .tax('$ 7.50')
+        .total('$ 90.95')
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/pricing/', 'More Information')
+                .image('https://raw.githubusercontent.com/amido/azure-vector-icons/master/renders/microsoft-azure.png')
+        ]);
+}
+
+function createSigninCard(session) {
+    return new builder.SigninCard(session)
+        .text('BotFramework Sign-in Card')
+        .button('Sign-in', 'https://login.microsoftonline.com')
 }
 
 function getSampleCardImages(session) {
@@ -89,7 +156,6 @@ function getSampleCardActions(session) {
         builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
     ];
 }
-
 
 
 
