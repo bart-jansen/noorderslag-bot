facb46b21e228cc48ca870e21146fc672a95b49a
Bart Jansen <b@rtjansen.nl>
2017-01-05 13:40:49 +0100

    lazy

--- messages/index.js
+++ messages/index.js
@@ -82,23 +82,9 @@ function createCard(session) {
         .title('BotFramework Hero Card')
         .subtitle('Your bots — wherever your users are talking')
         .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
-        .images(getSampleCardImages(session))
-        .buttons(getSampleCardActions(session));
+        .images([builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')])
+        .buttons([builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')]);
 }
-
-
-function getSampleCardImages(session) {
-    return [
-        builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
-    ];
-}
-
-function getSampleCardActions(session) {
-    return [
-        builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
-    ];
-}
-
 
 
 if (useEmulator) {
