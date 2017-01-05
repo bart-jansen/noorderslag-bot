90a0a12e5c159c2c4a226b904087c05fc2437557
Bart Jansen <b@rtjansen.nl>
2017-01-05 13:23:55 +0100

    lazy

--- messages/index.js
+++ messages/index.js
@@ -45,7 +45,17 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
         if (results.response) {
             // ... save task
             var foundEvent = DataLayer.getArtist(results.response);
-            session.send("Ok... Found the '%s' band.", foundEvent.description);
+
+            // create the card based on selection
+            var card = createCard(event, session);
+
+            // attach the card to the reply message
+            var msg = new builder.Message(session).addAttachment(card);
+            session.send(msg);
+
+
+
+            // session.send("Ok... Found the '%s' band.", foundEvent.description);
         } else {
             session.send("Ok");
         }
@@ -58,6 +68,19 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     });
 
 bot.dialog('/', intents);
+
+
+function createCard(event, session) {
+    return new builder.HeroCard(session)
+        .title('BotFramework Hero Card')
+        .subtitle('Your bots — wherever your users are talking')
+        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .images([builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')])
+        .buttons([builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')]);
+}
+
+
+
 
 if (useEmulator) {
     var restify = require('restify');
