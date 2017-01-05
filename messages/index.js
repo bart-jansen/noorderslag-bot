047c3a0ea717c17f4a9c0f5bdb397acebe422f30
Bart Jansen <b@rtjansen.nl>
2017-01-05 13:44:38 +0100

    lazy

--- messages/index.js
+++ messages/index.js
@@ -27,7 +27,27 @@ var luisAPIHostName = process.env.LuisAPIHostName || 'api.projectoxford.ai';
 const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
 
 //load json
-var DataLayer = require('./Data');
+var fs = require("fs");
+var didYouMean = require('didyoumean');
+
+var days = ['wednesday', 'thursday', 'friday', 'saturday'];
+
+var events = [];
+days.forEach(function(day) {
+     var dayContents = fs.readFileSync(__dirname + '/data/'+day+'.json');
+     var dayJSON = JSON.parse(dayContents)[0];
+
+     dayJSON.locations.forEach(function(loc) {
+        loc.events.forEach(function(event) {
+            events.push(event);
+        });
+    });
+});
+
+function getArtist(artistName) {
+    didYouMean.returnWinningObject = true;
+    return  didYouMean(artistName, events, 'description');
+}
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
@@ -43,7 +63,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     function (session, results) {
         if (results.response) {
             // // ... save task
-            var eventData = DataLayer.getArtist(results.response);
+            var eventData = getArtist(results.response);
 
             // // create the card based on selection
             // var card = createCard(eventData, session);
