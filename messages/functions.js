fec9a0f5717ddd098c8096f61130657516e0c19b
Bart Jansen <b@rtjansen.nl>
2017-01-13 13:12:41 +0100

    test

--- messages/functions.js
+++ messages/functions.js
@@ -5,7 +5,6 @@ var events = JSON.parse(eventContents);
 
 var venues = ['3FM stage - Ebbingekwartier','De Oosterpoort Benedenzaal 1 - Kelder','De Oosterpoort Foyer Grote Zaal','De Oosterpoort Grote Zaal','De Oosterpoort Kleine Zaal','De Oosterpoort Restaurant - Marathonzaal','Grand Theatre main','Grand Theatre up','Huize Maas front','Huize Maas main','Mutua Fides','Vera'];
 
-
 function searchEventByVenue(venue) {
     var foundEvents = [];
     events.forEach(function(event) {
@@ -17,7 +16,19 @@ function searchEventByVenue(venue) {
     return foundEvents;
 }
 
+function searchVenue(searchString) {
+    var venueList = [];
+
+    for(var i = 0; i < venues.length; i++) {
+        if(venues[i].toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
+            venueList.push(venues[i]);
+        }
+    }
+
+    return venueList;
+}
 
 module.exports = {
     searchEventByVenue: searchEventByVenue
+    searchVenue: searchVenue
 }
--- messages/index.js
+++ messages/index.js
@@ -82,17 +82,6 @@ function getArtist(artistName) {
 
     return returnVal;
 }
-function searchVenue(searchString) {
-    var venueList = [];
-
-    for(var i = 0; i < venues.length; i++) {
-        if(venues[i].toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
-            venueList.push(venues[i]);
-        }
-    }
-
-    return venueList;
-}
 
 function findEvents(searchTime, endTime) {
     var foundEvents = [];
@@ -207,7 +196,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
         }
         else {
             session.send('venue search' + session.dialogData.data.venue);
-            var venueSearch = searchVenue(session.dialogData.data.venue.toString());
+            var venueSearch = functions.searchVenue(session.dialogData.data.venue.toString());
             session.send(JSON.stringify(venueSearch));
 
             if(venueSearch.length === 1) {
